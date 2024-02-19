857e94592f0dd1719e8ed58002adc512275d075d
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-19 22:16:10 +0530

    best way to hadle error in react route

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,11 +1,26 @@
 import React from "react";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
 
 function Error() {
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Something went wrong";
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+  if (error.status === 400) {
+    title = "Not found";
+    message = "Could not find resource or page";
+  }
   return (
-    <div>
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
-    </div>
+    <>
+      <PageContent title={title}>
+        <p>{message}</p>
+        <p>Please try again later.</p>
+      </PageContent>
+    </>
   );
 }
 
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,12 +1,12 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
 
   const events = data.events;
   return <EventsList events={events} />;
@@ -15,11 +15,20 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventssss");
 
   if (!response.ok) {
     // ...
-    return { isError: true, message: "Could not fetch Events" };
+    // return { isError: true, message: "Could not fetch Events" };
+
+    // throw new Error("Could not fetch Events");
+    // it will render the nearest error element
+
+    // throw new response(JSON.stringify({ messgae: "Could not fetch events" }), {
+    //   status: 500,
+    // });
+
+    throw json({ message: "Error in fetching the events" }, { status: 500 });
   } else {
     return response;
     // const resData = await response.json();
