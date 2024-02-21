138516571cb5f8c47319a901b4f4242b016e5c6d
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-21 20:35:00 +0530

    setting priority in defer using await

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -72,7 +72,7 @@ async function loadEvents() {
 export async function loader({ request, params }) {
   const id = params.eventId;
   return defer({
-    event: loadEvent(id),
+    event: await loadEvent(id),
     events: loadEvents(),
   });
 }
