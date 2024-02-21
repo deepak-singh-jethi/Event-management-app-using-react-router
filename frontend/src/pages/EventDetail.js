541a4a2f2823f21097b219a954f3839096a84248
deepak-singh-jethi <deepujethi@yahoo.com>
2024-02-21 20:34:15 +0530

    added multiple loader in defer and used Await and Suspence

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
+  defer,
   json,
   redirect,
-  useLoaderData,
   useRouteLoaderData,
+  Await,
 } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 function EventDetail() {
   // const data = useLoaderData();
   // useRouteLoaderData("event-detail") enables children to use loader of parent
 
-  const data = useRouteLoaderData("event-detail");
-  const event = data.event;
+  const { event, events } = useRouteLoaderData("event-detail");
+
   return (
     <>
-      <EventItem event={event}></EventItem>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent}></EventItem>}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents}></EventsList>}
+        </Await>
+      </Suspense>
     </>
   );
 }
 
 export default EventDetail;
 
-// this loader is used by both eventdetail page and edit event page
-export async function loader({ request, params }) {
-  const id = params.eventId;
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
@@ -33,8 +43,38 @@ export async function loader({ request, params }) {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+// this loader is used by both eventdetail page and edit event page
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 // this action is used by both eventdetail
